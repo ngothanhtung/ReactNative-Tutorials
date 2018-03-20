4305f646f6f92928917b765204b307291e5102b7
Ngo Thanh Tung <tungnt@softech.vn>
2018-03-20 11:54:17 +0700

    update login screen

--- ReactNativeStarter/screens/LoginScreen.js
+++ ReactNativeStarter/screens/LoginScreen.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
+import { StyleSheet, View, Text, TextInput, Button, Dimensions } from 'react-native';
+
+var {width, height} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: '#ffffff'
   },
   inputText: {
-    width: 240,
+    width: width - 60,
     height: 48,
+
+  },
+  headerText: {
+    fontSize: 24,
+    color: '#2c3e50',
+    marginBottom: 12
+  },
+  hr: {
+    borderBottomColor: '#57606f',
+    borderBottomWidth: 1,
+    width: width - 60,
+    marginBottom: 24
   }
 });
 
@@ -36,24 +51,25 @@ class LoginScreen extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          Username:
-        </Text>
+        <Text style={styles.headerText}>
+          LOGIN SCREEN
+      </Text>
+      <View style={styles.hr}>
+      
+      </View>
+
         <TextInput
           style={styles.inputText}
-          placeholder=""
+          placeholder="Enter your username"
           onChangeText={(text) => this.setState({ username: text })}
         />
-        <Text>
-          Password:
-        </Text>
         <TextInput
           secureTextEntry={true}
           style={styles.inputText}
-          placeholder=""
+          placeholder="Enter your password"
           onChangeText={(text) => this.setState({ password: text })}
         />
-        <View style={{ paddingTop: 24}}>
+        <View style={{ paddingTop: 24 }}>
           <Button title="Login" onPress={this.onPressLogin} />
         </View>
       </View>
