32a548d66c9f4a0279b99ab0c8bfa64a7eb71476
Ngo Thanh Tung <tung.ngo@outlook.com>
2018-04-24 22:17:23 +0700

    Update ESLint

--- ReactNativeStarter/.eslintrc.js
+++ ReactNativeStarter/.eslintrc.js
@@ -2,5 +2,5 @@ module.exports = {
 	"extends": "airbnb",
 	"rules": {
 		"react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
-	}
+	},
 };
--- ReactNativeStarter/screens/LoginScreenV1/index.js
+++ ReactNativeStarter/screens/LoginScreenV1/index.js
@@ -1,91 +1,91 @@
 import React, { Component } from 'react';
 import {
-  View, Text, TextInput, Button, Image,
-  StyleSheet, Dimensions, TouchableOpacity
+  Alert, View, Text, TextInput, Image,
+  StyleSheet, Dimensions, TouchableOpacity,
 } from 'react-native';
 
-var { width, height } = Dimensions.get('window');
+import AptechLogoImage from './resources/aptech-logo.jpg';
 
-const AptechLogo = () => (
-  <Image source={require('../resources/aptech-logo.jpg')} resizeMode={'contain'} style={styles.logo} />
-)
+const { width } = Dimensions.get('window');
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#ffffff'
+    backgroundColor: '#ffffff',
   },
 
   footerContainer: {
     marginTop: 20,
     flexDirection: 'row',
-    marginBottom: 40
+    marginBottom: 40,
   },
 
   inputText: {
     width: width - 60,
     height: 48,
-    backgroundColor: '#f1f2f6',
+    backgroundColor: '#dfe6e9',
     color: '#0c2461',
     padding: 12,
     marginBottom: 8,
   },
   buttonContainer: {
-    backgroundColor: '#0c2461',
-    paddingVertical: 15
+    backgroundColor: '#2d3436',
+    paddingVertical: 15,
   },
   buttonText: {
     color: '#fff',
     textAlign: 'center',
-    fontWeight: '700'
+    fontWeight: '700',
   },
   loginButton: {
-    backgroundColor: '#0c2461',
     color: '#fff',
-    width: 240
+    width: 240,
   },
   signUpText: {
     color: '#0c2461',
     textDecorationLine: 'underline',
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
   headerText: {
     fontSize: 24,
     color: '#0c2461',
-    marginBottom: 12
+    marginBottom: 12,
   },
   hr: {
     borderBottomColor: '#0c2461',
     borderBottomWidth: 1,
     width: width - 60,
-    marginBottom: 24
+    marginBottom: 24,
   },
 
   logo: {
     width: 240,
     height: 175,
-    alignSelf: 'center'
+    alignSelf: 'center',
   },
 });
+
+const AptechLogo = () => (
+  <Image source={AptechLogoImage} resizeMode="contain" style={styles.logo} />
+);
 
 class LoginScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
       username: '',
-      password: ''
-    }
+      password: '',
+    };
   }
 
-  onPressLogin = () => {
-    var username = this.state.username;
-    var password = this.state.password;
+  onPressLogin() {
+    const { username, password } = this.state;
     if (username === 'admin' && password === '123456789') {
-      alert("Login OK");
+      Alert.alert('React Native', 'Login OK');
     } else {
-      alert("Login Failed");
+      Alert.alert('React Native', 'Login Failed');
     }
   }
 
@@ -96,47 +96,42 @@ class LoginScreen extends Component {
         <View>
           <Text style={styles.headerText}>
             STUDENT LOGIN
-        </Text>
-          <View style={styles.hr}>
-          </View>
+          </Text>
+          <View style={styles.hr} />
           <TextInput
             style={styles.inputText}
             placeholder="Enter your email"
-            placeholderTextColor="#6a89cc"
             keyboardType="email-address"
             autoCapitalize="none"
             autoCorrect={false}
             returnKeyType="next"
-            underlineColorAndroid={"transparent"}
-            onChangeText={(text) => this.setState({ username: text })}
+            underlineColorAndroid="transparent"
+            onChangeText={text => this.setState({ username: text })}
             onSubmitEditing={() => this.passwordInput.focus()}
           />
           <TextInput
-            ref={(input) => this.passwordInput = input}
-            secureTextEntry={true}
+            ref={(input) => { this.passwordInput = input; }}
+            secureTextEntry
             style={styles.inputText}
             placeholder="Enter your password"
-            placeholderTextColor="#6a89cc"
             returnKeyType="go"
-            underlineColorAndroid={"transparent"}
-            onChangeText={(text) => this.setState({ password: text })}
+            underlineColorAndroid="transparent"
+            onChangeText={text => this.setState({ password: text })}
           />
           <View>
-            {/*
-              <Button title="Login" onPress={this.onPressLogin} />
-            */}
-            <TouchableOpacity style={styles.buttonContainer} onPress={this.onPressLogin}>
+            <TouchableOpacity
+              style={styles.buttonContainer}
+              onPress={() => { this.onPressLogin(); }}
+            >
               <Text style={styles.buttonText}>LOGIN</Text>
             </TouchableOpacity>
           </View>
         </View>
         <View style={styles.footerContainer}>
           <Text>Have an account? </Text>
-          <TouchableOpacity onPress={() => { alert("This feature is coming soon (Next chapter)") }}>
+          <TouchableOpacity onPress={() => { Alert.alert('React Native', 'This feature is coming soon (Next chapter)'); }}>
             <Text style={styles.signUpText}>Sign Up</Text>
           </TouchableOpacity>
-
-
         </View>
       </View>
     );
