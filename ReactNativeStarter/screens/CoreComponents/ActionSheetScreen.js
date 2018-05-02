4fa1f065e09d5a2d108fbcf1a445a4379ad1049f
Ngo Thanh Tung <tungnt@softech.vn>
2018-05-02 14:17:27 +0700

    Update ActionSheetIOS

---
+++ ReactNativeStarter/screens/CoreComponents/ActionSheetScreen.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { View, Text, } from 'react-native';
+
+export default class ActionSheetScreen extends Component {
+  render() {
+    return (
+      <View>
+        <Text> ActionSheetScreen </Text>
+      </View>
+    );
+  }
+}
---
+++ ReactNativeStarter/screens/CoreComponents/IOS/DatePickerIOSScreen.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import {
+  DatePickerIOS,
+  View,
+  StyleSheet,
+} from 'react-native'
+
+export default class DatePickerIOSScreen extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { chosenDate: new Date() };
+
+    this.setDate = this.setDate.bind(this);
+  }
+
+  setDate(newDate) {
+    this.setState({ chosenDate: newDate })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <DatePickerIOS
+          date={this.state.chosenDate}
+          onDateChange={this.setDate}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  },
+})
