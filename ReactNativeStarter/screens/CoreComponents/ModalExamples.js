d9bdc7d33d8690f76e5051ec7a0f9cf2548562b1
Ngo Thanh Tung <tungnt@softech.vn>
2018-05-03 14:44:26 +0700

    Update ModalExamples

--- ReactNativeStarter/screens/CoreComponents/ModalExamples.js
+++ ReactNativeStarter/screens/CoreComponents/ModalExamples.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react';
 import { Alert, Modal, Text, TextInput, TouchableHighlight, View } from 'react-native';
 
-export default class ModalExamples extends Component {
+class TextInputWithModal extends Component {
   constructor(props) {
     super(props);
     this.state = {
       modalVisible: false,
-    };
-  }
-
-  setModalVisible(visible) {
-    this.setState({ modalVisible: visible });
+      selectedValue: '',
+    }
   }
 
   render() {
     return (
-      <View style={{ marginTop: 22 }}>
+      <View>
         <Modal
           animationType="slide"
           transparent={true}
           visible={this.state.modalVisible}
           onRequestClose={() => {
-            Alert.alert('Modal has been closed.');
+            // ANDROID: WHEN HARDWARE BACK BUTTON WAS PRESSED
+            this.setState({ modalVisible: false });
           }}
         >
           <View
@@ -34,9 +32,9 @@ export default class ModalExamples extends Component {
                 <View style={{ flex: 1, alignItems: 'flex-start', backgroundColor: 'yellow' }}>
                   <TouchableHighlight
                     style={{
-                      height: 32, backgroundColor: '#2c3e50', alignItems: 'center', justifyContent: 'center', width: 60
+                      height: 40, backgroundColor: '#2c3e50', alignItems: 'center', justifyContent: 'center', width: 60
                     }}
-                    onPress={() => { this.setModalVisible(!this.state.modalVisible); }}
+                    onPress={() => { this.setState({ modalVisible: false }); }}
                   >
                     <Text style={{ color: '#ffffff' }}>Cancel</Text>
                   </TouchableHighlight>
@@ -44,9 +42,16 @@ export default class ModalExamples extends Component {
                 <View style={{ flex: 1, alignItems: 'flex-end', backgroundColor: 'orange' }}>
                   <TouchableHighlight
                     style={{
-                      height: 32, backgroundColor: '#2c3e50', alignItems: 'center', justifyContent: 'center', width: 60
+                      height: 40, backgroundColor: '#2c3e50', alignItems: 'center', justifyContent: 'center', width: 60
                     }}
-                    onPress={() => { this.setModalVisible(!this.state.modalVisible); }}
+                    onPress={() => {
+                      // HARD VALUE
+                      var value = 'This is value';
+                      this.setState({ selectedValue: value });
+                      this.props.onSelect(value);
+                      // CLOSE MODAL
+                      this.setState({ modalVisible: false });
+                    }}
                   >
                     <Text style={{ color: '#ffffff' }}>Select</Text>
                   </TouchableHighlight>
@@ -54,23 +59,43 @@ export default class ModalExamples extends Component {
               </View>
             </View>
 
-            <View style={{ top: 32, position: 'absolute', width: '100%', height: '100%' }}>
+            <View style={{ top: 40, position: 'absolute', width: '100%', height: '100%' }}>
               <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#fab1a0' }}>
                 <Text>Content here</Text>
               </View>
             </View>
-
-
           </View>
         </Modal>
         {/* ----------------------------------------------------------------------------------- */}
+        <TouchableHighlight onPress={() => { this.setState({ modalVisible: true }); }}>
+          <TextInput
+            style={{ height: 48, backgroundColor: '#dfe6e9', paddingLeft: 12, paddingRight: 12 }}
+            placeholder="Enter your text"
+            underlineColorAndroid="transparent"
+            editable={false}
+            value={this.state.selectedValue}
+          />
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
 
-        <TextInput
-          style={{ height: 48, backgroundColor: '#dfe6e9', paddingLeft: 12, paddingRight: 12 }}
-          placeholder="Enter your text"
-          underlineColorAndroid="transparent"
-          onTouchStart={() => { this.setModalVisible(true); }}
-        />
+export default class ModalExamples extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+    }
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <TextInputWithModal onSelect={(value) => { this.setState({ text: value }) }} />
+        <Text>
+          {this.state.text}
+        </Text>
       </View>
     );
   }
