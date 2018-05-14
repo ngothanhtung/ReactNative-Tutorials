03cc09fc02496c00e41bcc8c6d9ae374f915e443
Ngo Thanh Tung <tungnt@softech.vn>
2018-05-14 09:33:49 +0700

    Update packages

--- ReactNativeOnlineShop/src/navigators/StackNavigator.js
+++ ReactNativeOnlineShop/src/navigators/StackNavigator.js
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
 import { createStackNavigator } from 'react-navigation';
 import LoginScreen from '../screens/LoginScreen';
+import AnimationScreen from '../screens/AnimationScreen';
+import LoadingScreen from '../screens/LoadingScreen';
 
 export default createStackNavigator({
-  LoginScreen: LoginScreen
+  LoadingScreen: {
+    screen: LoadingScreen,
+    navigationOptions: ({ navigation }) => ({
+      title: 'Loading',
+      header: null
+    }),
+  },
+  LoginScreen: LoginScreen,
+  AnimationScreen: AnimationScreen,
 });
