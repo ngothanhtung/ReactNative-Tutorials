45e8bfdc164b570177e92d8049a9d1f74a17919f
Ngo Thanh Tung <tung.ngo@outlook.com>
2018-07-05 15:05:23 +0700

    Update Merge

--- NativeBaseStarter/index.js
+++
@@ -1,8 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-AppRegistry.registerComponent('nativebasestarter', () => App);
-
-import ButtonExamples from './src/screens/ButtonExamples';
-AppRegistry.registerComponent('nativebasestarter', () => ButtonExamples);
-
-
