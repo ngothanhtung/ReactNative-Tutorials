47216b7d7a11a294460dd53dd537af786c64e285
Ngo Thanh Tung <tung.ngo@outlook.com>
2018-07-30 09:12:03 +0700

    Update Status Bar

--- ReactNativeFinalProject/App.js
+++ ReactNativeFinalProject/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Platform, StatusBar } from 'react-native';
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunkMiddleware from 'redux-thunk';
@@ -28,6 +29,14 @@ const store = createStore(
 );
 
 export default class App extends React.Component {
+  componentWillMount() {
+    if (Platform.OS == 'android') {
+      StatusBar.setBackgroundColor('#FFC600', true);
+    }
+
+    StatusBar.setBarStyle('light-content', true);
+  }
+
   render() {
     return (
       <Provider store={store}>
