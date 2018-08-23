e3d71257c41be1af1d7f9593127e0ca0811027a5
Ngo Thanh Tung <tungnt@softech.vn>
2018-08-23 21:41:01 +0700

    UI

--- ReactNative-Batch03-Projects/AwesomeProject/src/StyleExample2.js
+++ ReactNative-Batch03-Projects/AwesomeProject/src/StyleExample2.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, ScrollView } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
 
 export default class StyleExample2 extends Component {
   constructor(props) {
@@ -11,10 +11,12 @@ export default class StyleExample2 extends Component {
   render() {
     return (
       <View style={{ flex: 1, backgroundColor: 'yellow' }}>
-        <View style={{ height: 32, backgroundColor: 'white' }}></View>
+        <View style={{ height: 24, backgroundColor: 'white' }}></View>
         <View style={{ height: 60, backgroundColor: 'violet', flexDirection: 'row' }}>
           <View style={{ width: 60, height: 60, backgroundColor: 'yellow' }}></View>
-          <View style={{ flex: 1 }}></View>
+          <View style={{ flex: 1, justifyContent: 'flex-end', padding: 8 }}>
+            <Text>Lorem Ipsum is simply</Text>
+          </View>
           <View style={{ width: 60, height: 60, backgroundColor: 'yellow' }}></View>
         </View>
         <ScrollView style={{ flex: 1, backgroundColor: '#ecf0f1' }}>
@@ -25,8 +27,18 @@ export default class StyleExample2 extends Component {
                   <View style={{ height: 80, width: 80, opacity: (index + 2) * 0.15, backgroundColor: '#8e44ad', justifyContent: 'center', alignItems: 'center' }}>
                     <View style={{ height: 48, width: 48, backgroundColor: 'white', borderRadius: 24 }}></View>
                   </View>
-                  <View style={{ height: 80, flex: 1, opacity: (index + 2) * 0.15, backgroundColor: '#27ae60' }}></View>
-                  <View style={{ height: 80, width: 80, opacity: (index + 2) * 0.15, backgroundColor: '#2c3e50' }}></View>
+                  <View style={{ height: 80, flex: 1, opacity: (index + 2) * 0.15, backgroundColor: '#27ae60' }}>
+                    <View style={{flex:1, backgroundColor: 'red', justifyContent: 'center', padding: 8 }}>
+                      <Text>Lorem Ipsum is simply</Text>
+                    </View>
+                    <View style={{flex:2, backgroundColor: 'orange', paddingLeft: 8}}>                      
+                      <Text>Lorem Ipsum is simply dummy text of the printing</Text>
+                    </View>
+                  
+                  </View>
+                  <View style={{ height: 80, width: 80, opacity: (index + 2) * 0.15, backgroundColor: '#2c3e50', justifyContent: 'center', alignItems: 'center' }}>
+                    <View style={{ height: 36, width: 36, backgroundColor: 'white', borderRadius: 24 }}></View>
+                  </View>
                 </View>
                 {/* ------------------------------------------------------------------ */}
                 <View style={{ height: 1 }}></View>
@@ -34,17 +46,17 @@ export default class StyleExample2 extends Component {
             ))
           }
         </ScrollView>
-        <View style={{ height: 60, backgroundColor: 'orange', flexDirection: 'row' }}>
-          <View style={{ flex: 1, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ height: 60, flexDirection: 'row' }}>
+          <View style={{ flex: 1, backgroundColor: 'orange', justifyContent: 'center', alignItems: 'center' }}>
             <View style={{ height: 48, width: 48, backgroundColor: 'white', borderRadius: 24 }}></View>
           </View>
-          <View style={{ flex: 1, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' }}>
+          <View style={{ flex: 1, backgroundColor: 'orange', justifyContent: 'center', alignItems: 'center' }}>
             <View style={{ height: 48, width: 48, backgroundColor: 'white', borderRadius: 24 }}></View>
           </View>
-          <View style={{ flex: 1, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' }}>
+          <View style={{ flex: 1, backgroundColor: 'orange', justifyContent: 'center', alignItems: 'center' }}>
             <View style={{ height: 48, width: 48, backgroundColor: 'white', borderRadius: 24 }}></View>
           </View>
-          <View style={{ flex: 1, backgroundColor: 'red', justifyContent: 'center', alignItems: 'center' }}>
+          <View style={{ flex: 1, backgroundColor: 'orange', justifyContent: 'center', alignItems: 'center' }}>
             <View style={{ height: 48, width: 48, backgroundColor: 'white', borderRadius: 24 }}></View>
           </View>
         </View>
