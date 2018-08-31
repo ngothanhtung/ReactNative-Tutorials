d333d70335480ff6ed6587607c26fe8acf88019b
Ngo Thanh Tung <tungnt@softech.vn>
2018-08-31 18:30:30 +0700

    Update FLEX

--- ReactNative-Batch03-Projects/AwesomeProject/index.js
+++ ReactNative-Batch03-Projects/AwesomeProject/index.js
@@ -5,6 +5,7 @@ import App from './src/App';
 import StyleExample from './src/StyleExample';
 import StyleExample2 from './src/StyleExample2';
 import HandlingTextInput from './src/HandlingTextInput';
+import FlexLayoutExamples from './src/FlexLayoutExamples';
 import { name as appName } from './src/app.json';
 
-AppRegistry.registerComponent(appName, () => HandlingTextInput);
+AppRegistry.registerComponent(appName, () => FlexLayoutExamples);
