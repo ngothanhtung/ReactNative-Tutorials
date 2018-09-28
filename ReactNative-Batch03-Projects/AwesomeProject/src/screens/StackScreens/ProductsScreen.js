3ed166476443f2855477f46a6ca5ad3e2ab90eb3
Ngo Thanh Tung <tungnt@softech.vn>
2018-09-28 19:58:46 +0700

    Update NativeBase

--- ReactNative-Batch03-Projects/AwesomeProject/src/screens/StackScreens/ProductsScreen.js
+++ ReactNative-Batch03-Projects/AwesomeProject/src/screens/StackScreens/ProductsScreen.js
@@ -1,27 +1,35 @@
 import React, { Component } from 'react';
-import { Text, View, FlatList, Image, TouchableOpacity } from 'react-native';
+import { View, FlatList, Image, TouchableOpacity } from 'react-native';
 import axios from 'axios';
 import Star from '../../components/shared/Star';
+
+import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right } from 'native-base';
 
 const url = 'https://slacklivechat.com/jsonplaceholder/products';
 
 class Product extends Component {
   render() {
     return (
-      <TouchableOpacity onPress={() => {
-        const id = this.props.product.id;
-        this.props.navigation.navigate('ProductDetailsScreen', { id: id })
-        // alert(this.props.product.id);
-      }}>
-        <View style={{ padding: 4 }}>
-          <Image source={{ uri: this.props.product.imageUrl }} style={{ height: 120, width: 120 }} />
-          <Text>
-            {this.props.product.name}
-          </Text>
-          <Star number={3} />
-        </View>
-      </TouchableOpacity>
 
+      <Card>
+        <CardItem>
+          <Body>
+            <Text>{this.props.product.name}</Text>
+          </Body>
+        </CardItem>
+        <CardItem cardBody>
+          <Image source={{ uri: this.props.product.imageUrl }} style={{ height: 200, width: null, flex: 1 }} />             
+        </CardItem>
+        <CardItem>
+
+          <Body>
+            <Text>{this.props.product.price}</Text>
+          </Body>
+          <Right>
+            <Star number={3} />
+          </Right>
+        </CardItem>
+      </Card>
     );
   }
 }
@@ -44,15 +52,18 @@ export default class ProductsScreen extends Component {
 
   render() {
     return (
-      <View>
-        <FlatList
-          data={this.state.products}
-          keyExtractor={(item, index) => index.toString()}
-          renderItem={({ item }) =>
-            <Product navigation={this.props.navigation} product={item} />
-          }
-        />
-      </View>
+      <Container>
+        <Content style={{ padding: 12 }}>
+          <FlatList
+            data={this.state.products}
+            keyExtractor={(item, index) => index.toString()}
+            renderItem={({ item }) =>
+              <Product navigation={this.props.navigation} product={item} />
+            }
+          />
+        </Content>
+
+      </Container>
     )
   }
 }
