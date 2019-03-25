a333ceaeb97f211834eef958a4f703d37e4c4241
Ngo Thanh Tung <tungnt@softech.vn>
2019-03-25 19:51:25 +0700

    Update API version 2

--- FinalProjects/ExpressServer/routes/categories.js
+++ FinalProjects/ExpressServer/routes/categories.js
@@ -14,7 +14,7 @@ router.get('/', function(req, res, next) {
 		});
 });
 
-// Example: GET: http://localhost:3000/products
+// Example: GET: http://localhost:3000/categories/5ba1c1b80a456c86100cc95a/products
 router.get('/:categoryId/products', function(req, res, next) {
 	const categoryId = req.params.categoryId;
 	MongoDbHelper.findDocuments({ categoryId: ObjectID(categoryId) }, 'products', {})
