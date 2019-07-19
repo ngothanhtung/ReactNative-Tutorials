d12f6929a760f246030e095bb42c245c7cd3ecf3
Ngo Thanh Tung <tungnt@softech.vn>
2019-07-19 19:04:45 +0700

    UPDATE

--- Batch05/Jobs/src/modules/WorkItem/workitemSagas.js
+++ Batch05/Jobs/src/modules/WorkItem/workitemSagas.js
@@ -4,26 +4,24 @@ import axios from 'axios';
 import { Alert } from 'react-native';
 // FIREBASE
 import firebase from 'react-native-firebase';
+
 const db = firebase.firestore();
 // ====================================================================================================================
 // get from filestore
 function* getWorkItemsAsync(action) {
-  yield put({ type: ActionTypes.GET_WORKITEMS_PENDING });
-  db.collection('WorkItems')
-    .get()
-    .then((snapshot) => {
-      var workitems = [];
-      snapshot.forEach((doc) => {
-        const workitem = doc.data();
-        workitems.push(workitem);
-      });
-      dispatch({ type: ActionTypes.GET_WORKITEMS_SUCCESS, workitems });
-      // yield put({type: ActionTypes.GET_WORKITEMS_SUCCESS, workitems});
-    })
-    .catch((error) => {
-      // yield put({type:ActionTypes.GET_WORKITEMS_ERROR, error});
-      console.log('Error getting documents', err);
+  try {
+    yield put({ type: ActionTypes.GET_WORKITEMS_PENDING });
+    const snapshot = yield db.collection('WorkItems').get();
+    var workitems = [];
+    snapshot.forEach((doc) => {
+      const workitem = doc.data();
+      workitems.push(workitem);
     });
+    yield put({ type: ActionTypes.GET_WORKITEMS_SUCCESS, workitems });
+  } catch (error) {
+    yield put({ type: ActionTypes.GET_WORKITEMS_ERROR, error });
+    console.log('Error getting documents', err);
+  }
 }
 
 // ====================================================================================================================
