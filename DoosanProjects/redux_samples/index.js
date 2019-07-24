7e32a3280d78b01033b25a7aa552c4f02fc438a3
Ngo Thanh Tung <tungnt@softech.vn>
2019-07-24 15:11:40 +0700

    Update

--- DoosanProjects/redux_samples/index.js
+++ DoosanProjects/redux_samples/index.js
@@ -4,6 +4,7 @@
 
 import { AppRegistry } from 'react-native';
 import App from './src/App';
+import Session08 from './src/session08';
 import { name as appName } from './src/app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Session08);
