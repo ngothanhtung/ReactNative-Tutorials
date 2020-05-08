9749c5c7a57abce4bf71169c8bcd8d9bcbacefb2
Ngo Thanh Tung <tungnt@softech.vn>
2020-05-08 19:35:17 +0700

    Update Gallery

--- Batch07/my-react-web/src/Gallery.js
+++ Batch07/my-react-web/src/Gallery.js
@@ -12,9 +12,10 @@ export default class Gallery extends Component {
   render() {
     return (
       <div>
-        <img src={'images/' + this.state.number + '.jpg'} style={{ width: 500 }} />
+        <img alt='' src={'images/' + this.state.number + '.jpg'} style={{ width: 500 }} />
         <p>
           <button
+            disabled={this.state.number === 1 ? 'disable' : ''}
             onClick={() => {
               //
               // this.state.number = this.state.number + 1;
@@ -24,7 +25,9 @@ export default class Gallery extends Component {
           >
             Back
           </button>
+          <span>{this.state.number} / 3</span>
           <button
+            disabled={this.state.number === 3 ? 'disable' : ''}
             onClick={() => {
               const newNumber = this.state.number + 1;
               this.setState({ number: newNumber });
@@ -33,6 +36,29 @@ export default class Gallery extends Component {
             Next
           </button>
         </p>
+        <div>
+          <img
+            src='/images/1.jpg'
+            style={{ height: 60, opacity: this.state.number === 1 ? 1 : 0.5 }}
+            onClick={() => {
+              this.setState({ number: 1 });
+            }}
+          />
+          <img
+            src='/images/2.jpg'
+            style={{ height: 60, opacity: this.state.number === 2 ? 1 : 0.5 }}
+            onClick={() => {
+              this.setState({ number: 2 });
+            }}
+          />
+          <img
+            src='/images/3.jpg'
+            style={{ height: 60, opacity: this.state.number === 3 ? 1 : 0.5 }}
+            onClick={() => {
+              this.setState({ number: 3 });
+            }}
+          />
+        </div>
       </div>
     );
   }
