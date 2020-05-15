1284d6028e6d78ce058d284498f13cd6801ceac9
Ngo Thanh Tung <tungnt@softech.vn>
2020-05-15 20:39:40 +0700

    Update LOGIN

--- Batch07/AwesomeProject/index.js
+++ Batch07/AwesomeProject/index.js
@@ -5,6 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import FlexLayoutExample from './FlexLayoutExample';
+import Login from './Login';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => FlexLayoutExample);
+AppRegistry.registerComponent(appName, () => Login);
