9f1e52aacc1c4c88d52944d9a6678e79485046c4
Ngo Thanh Tung <tungnt@softech.vn>
2020-05-18 20:22:02 +0700

    Update UI Sample - Insragram + TinhTe

--- Batch07/AwesomeProject/index.js
+++ Batch07/AwesomeProject/index.js
@@ -9,6 +9,7 @@ import Login from './Login';
 import ImageExample from './ImageExample';
 import ImageBackgroundExample from './ImageBackgroundExample';
 import SvgExample from './SvgExample';
+import InstagramExample from './InstagramExample';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => SvgExample);
+AppRegistry.registerComponent(appName, () => InstagramExample);
