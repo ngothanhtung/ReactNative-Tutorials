6aa1d7e90e77b52401a9b4e3a3111392e1f951fd
Ngo Thanh Tung <tungnt@softech.vn>
2020-07-01 16:34:05 +0700

    MERGE

--- 07.ReactNativeRedux/src/ReduxApp/Reducers/counterReducer.js
+++ 07.ReactNativeRedux/src/ReduxApp/Reducers/counterReducer.js
@@ -1,18 +1,18 @@
 import * as ActionTypes from '../Actions/types';
 
 const counterReducer = (state = { count: 0 }, action) => {
-	switch (action.type) {
-		case ActionTypes.INCREASE_COUNT:
-			return Object.assign({}, state, {
-				count: state.count + action.number
-			});
-		case ActionTypes.DECREASE_COUNT:
-			return Object.assign({}, state, {
-				count: state.count - action.number
-			});
-		default:
-			return state;
-	}
-}
+  switch (action.type) {
+    case ActionTypes.INCREASE_COUNT:
+      return Object.assign({}, state, {
+        count: state.count + action.number,
+      });
+    case ActionTypes.DECREASE_COUNT:
+      return Object.assign({}, state, {
+        count: state.count - action.number,
+      });
+    default:
+      return state;
+  }
+};
 
 export default counterReducer;
