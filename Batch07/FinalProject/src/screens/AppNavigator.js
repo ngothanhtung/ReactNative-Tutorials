2e05470b6f13e17ffbfeabffd720b97b2b1d2ba7
Ngo Thanh Tung <tungnt@softech.vn>
2020-07-10 18:17:27 +0700

    Update

--- Batch07/FinalProject/src/screens/AppNavigator.js
+++ Batch07/FinalProject/src/screens/AppNavigator.js
@@ -1,35 +1,43 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import {useSelector} from 'react-redux';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
-import LoginScreen from './LoginScreen';
-import RegisterScreen from './RegisterScreen';
-import TabNavigator from './TabNavigator';
+import LoginStackNavigator from './LoginStackNavigator';
+import MainTabNavigator from './MainTabNavigator';
 
 import * as routes from '../routes';
 const Stack = createStackNavigator();
 
+const AuthenticationStackNavigator = () => {
+  const loggedInUser = useSelector((state) => state.auth.loggedInUser);
+
+  if (!loggedInUser) {
+    return (
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen
+          name={routes.AUTH_LOGIN_STACK_NAVIGATOR}
+          component={LoginStackNavigator}
+        />
+      </Stack.Navigator>
+    );
+  }
+
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false}}>
+      <Stack.Screen
+        name={routes.MAIN_TAB_NAVIGATOR}
+        component={MainTabNavigator}
+      />
+    </Stack.Navigator>
+  );
+};
+
 const AppNavigator = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name={routes.AUTH_LOGIN_SCREEN}
-          component={LoginScreen}
-          options={{title: 'Đăng nhập', headerShown: true}}
-        />
-        <Stack.Screen
-          name={routes.AUTH_REGISTER_SCREEN}
-          component={RegisterScreen}
-          options={{title: 'Đăng ký', headerShown: true}}
-        />
-        <Stack.Screen
-          name="TabNavigator"
-          component={TabNavigator}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
+      <AuthenticationStackNavigator />
     </NavigationContainer>
   );
 };
