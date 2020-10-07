765bd174d065703a98e50f31ce15471f46ffff1a
Ngo Thanh Tung <tungnt@softech.vn>
2020-10-07 21:12:00 +0700

    Update

--- Batch07/FinalProject/index.js
+++ Batch07/FinalProject/index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './src/app';
-import {name as appName} from './src/app/app.json';
+import { name as appName } from './src/app/app.json';
 
 AppRegistry.registerComponent(appName, () => App);
