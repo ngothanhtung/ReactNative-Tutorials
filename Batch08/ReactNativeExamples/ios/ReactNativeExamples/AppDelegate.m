56d99b5247825f39303cb8e3c8258eb9981d1f57
Ngo Thanh Tung <tungnt@softech.vn>
2020-12-19 19:10:09 +0700

    Update SESSION

--- Batch08/ReactNativeExamples/ios/ReactNativeExamples/AppDelegate.m
+++ Batch08/ReactNativeExamples/ios/ReactNativeExamples/AppDelegate.m
@@ -11,6 +11,7 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+#import <Firebase.h>
 
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
@@ -43,6 +44,9 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  
+  [FIRApp configure];
+
   return YES;
 }
 
