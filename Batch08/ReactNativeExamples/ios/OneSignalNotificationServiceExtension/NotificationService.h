2ed91894d269e0ddffc442f17658167774114a80
Ngo Thanh Tung <tungnt@softech.vn>
2021-01-09 07:05:23 +0700

    Update

---
+++ Batch08/ReactNativeExamples/ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Ngô Thanh Tùng on 1/9/21.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
