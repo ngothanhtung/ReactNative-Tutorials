215e476c7a9a3f2c217a156a62a4d56382c2d701
Ngo Thanh Tung <tungnt@softech.vn>
2021-04-23 00:21:18 +0700

    Update

---
+++ Batch08/FinalProject/src/screens/LoginStackNavigator/SignInWithPhoneScreen.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import SignInWithPhone from '../../modules/auth/components/SignInWithPhone';
+
+export default function SignInWithPhoneScreen({ navigation }) {
+  return <SignInWithPhone />;
+}
