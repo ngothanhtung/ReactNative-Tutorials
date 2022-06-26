2df6fc72d6bb278771f6ba192ddb1d126d344e7b
Ngô Thanh Tùng <tungnt@softech.vn>
2022-06-26 09:36:22 +0700

    Update Fonts

--- Batch09/Examples/App.tsx
+++ Batch09/Examples/App.tsx
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import * as Font from 'expo-font';
 import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import Menu from './src/Homeworks/Session02/Menu';
 import EffectExample from './src/Session03/EffectExample';
@@ -18,36 +19,56 @@ import SectionListExample from './src/Session05/SessionListExample';
 
 export default function App() {
   const [visible, setVisible] = React.useState(true);
+  const [fontsLoaded, setFontsLoaded] = React.useState(false);
+
+  const loadFonts = async () => {
+    await Font.loadAsync({
+      'SVN-Gilroy-Regular': require('./assets/fonts/SVN-Gilroy-Regular.otf'),
+      'SVN-Gilroy-Medium': require('./assets/fonts/SVN-Gilroy-Medium.otf'),
+      'SVN-Gilroy-SemiBold': require('./assets/fonts/SVN-Gilroy-SemiBold.otf'),
+      'SVN-Gilroy-Bold': require('./assets/fonts/SVN-Gilroy-Bold.otf'),
+    });
+
+    setFontsLoaded(true);
+  };
+
+  React.useEffect(() => {
+    loadFonts();
+  }, []);
 
   return (
-    <View style={styles.container}>
-      {/* <Header style={{ color: 'red', fontSize: 32 }} text='Hello' fontSize={24} status='waiting' />
-      <LikeButton />
-      <RateButton /> */}
-      {/* <Login /> */}
-      <StatusBar style='light' />
-      {/* <Menu /> */}
-      {/* <StateExample /> */}
-      {/* {visible && <EffectExample />} */}
+    <React.Fragment>
+      {fontsLoaded && (
+        <View style={styles.container}>
+          {/* <Header style={{ color: 'red', fontSize: 32 }} text='Hello' fontSize={24} status='waiting' />
+          <LikeButton />
+          <RateButton /> */}
+          {/* <Login /> */}
+          <StatusBar style='light' />
+          {/* <Menu /> */}
+          {/* <StateExample /> */}
+          {/* {visible && <EffectExample />} */}
 
-      {/* <Button
-        title='Hide component'
-        onPress={() => {
-          setVisible(!visible);
-        }}
-      /> */}
+          {/* <Button
+            title='Hide component'
+            onPress={() => {
+              setVisible(!visible);
+            }}
+          /> */}
 
-      {/* <StyleExample /> */}
-      {/* <BasicFlexBox /> */}
-      {/* <ImageExample /> */}
-      {/* <ScrollViewExample /> */}
-      {/* <ScrollViewWithManyDataExample /> */}
-      {/* <FlatListExample /> */}
-      {/* <SectionListExample /> */}
-      <SectionListAdvancedExample />
-      {/* <NetworkingExample /> */}
-      {/* <NetworkingWithAxios /> */}
-    </View>
+          {/* <StyleExample /> */}
+          {/* <BasicFlexBox /> */}
+          {/* <ImageExample /> */}
+          {/* <ScrollViewExample /> */}
+          {/* <ScrollViewWithManyDataExample /> */}
+          {/* <FlatListExample /> */}
+          {/* <SectionListExample /> */}
+          <SectionListAdvancedExample />
+          {/* <NetworkingExample /> */}
+          {/* <NetworkingWithAxios /> */}
+        </View>
+      )}
+    </React.Fragment>
   );
 }
 
