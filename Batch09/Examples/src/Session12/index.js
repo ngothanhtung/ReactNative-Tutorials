0a1da43c9606d58a78a0d953bbb4c18accc49671
Ngo Thanh Tung <tungnt@softech.vn>
2022-08-13 16:54:05 +0700

    Update

--- Batch09/Examples/src/Session12/index.js
+++ Batch09/Examples/src/Session12/index.js
@@ -1,4 +1,4 @@
-import { View, Text, Platform } from 'react-native';
+import { View, Text, Platform, SafeAreaView, Button } from 'react-native';
 import React from 'react';
 
 // OneSignal
@@ -7,13 +7,16 @@ import Constants from 'expo-constants';
 OneSignal.setAppId(Constants.manifest.extra.oneSignalAppId);
 
 export default function Session12() {
-  // promptForPushNotifications will show the native Android notification permission prompt.
-  // We recommend removing the following code and instead using an In-App Message to prompt for notification permission (See step 7)
-  if (Platform.OS === 'android') {
-    OneSignal.promptForPushNotifications();
-  }
+  React.useEffect(() => {
+    OneSignal.getDeviceState().then((deviceState) => {
+      console.log(deviceState);
+    });
+  }, []);
 
-  OneSignal.promptForPushNotificationsWithUserResponse();
+  OneSignal.promptForPushNotificationsWithUserResponse((response) => {
+    console.log('Prompt response:', response);
+  });
+
   //Method for handling notifications received while app in foreground
   OneSignal.setNotificationWillShowInForegroundHandler((notificationReceivedEvent) => {
     console.log('OneSignal: notification will show in foreground:', notificationReceivedEvent);
@@ -31,8 +34,66 @@ export default function Session12() {
   });
 
   return (
-    <View>
+    <SafeAreaView>
       <Text>Session12</Text>
-    </View>
+      <Button
+        title='Send'
+        onPress={() => {
+          const message = {
+            app_id: '590a207e-e23b-490b-a18d-8c337fb3f801',
+            include_player_ids: ['fe103930-d91e-47e0-b438-3dee5ef88664'],
+            // "included_segments": [""],
+            large_icon: 'ic_action_cloud_upload',
+            android_group: 'group-2',
+            android_group_message: {
+              en: 'You have $[notif_count] new messages',
+            },
+            // "collapse_id": "100",
+            thread_id: '100480',
+            summary_arg_count: 2,
+            summary_arg: '12 more notifications from John Doe',
+            data: {
+              foo: 'bar',
+            },
+            headings: {
+              en: 'Mobile Heading',
+            },
+            contents: {
+              en: 'Nội dung thông báo',
+            },
+            big_picture: 'https://images.unsplash.com/photo-1571713343936-b27b7bedddab?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+            ios_attachments: {
+              id1: 'https://images.unsplash.com/photo-1571713343936-b27b7bedddab?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80',
+            },
+            ios_badgeCount: '1',
+            ios_badgeType: 'Increase',
+            buttons: [
+              {
+                id: 'id1',
+                text: 'first button',
+                icon: 'ic_menu_share',
+              },
+              {
+                id: 'id2',
+                text: 'second button',
+                icon: 'ic_menu_send',
+              },
+            ],
+          };
+
+          const jsonString = JSON.stringify(message);
+
+          OneSignal.postNotification(
+            jsonString,
+            (success) => {
+              console.log('Success:', success);
+            },
+            (error) => {
+              console.log('Error:', error);
+            },
+          );
+        }}
+      />
+    </SafeAreaView>
   );
 }
