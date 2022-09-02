ea59d626d3c8c0b6d638731f88cdb5a8e0cb9e96
Ngô Thanh Tùng <tungnt@softech.vn>
2022-09-02 18:28:28 +0700

    Update Animation

--- Batch09/reactnative-animations/src/components/Reanimated/ReMovement.tsx
+++ Batch09/reactnative-animations/src/components/Reanimated/ReMovement.tsx
@@ -1,10 +1,28 @@
 import React from 'react';
-import { Button } from 'react-native';
+import { Button, SafeAreaView, Animated as A } from 'react-native';
 import Animated, { Easing, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 export default function ReMovement() {
+  const animatedSize = React.useRef(new A.Value(0)).current;
+
+  const animatedWidth = useSharedValue(0);
+  const animatedHeight = useSharedValue(0);
+
   const randomWidth = useSharedValue(0);
   const randomFontSize = useSharedValue(15);
+
+  const circleStyle = useAnimatedStyle(() => {
+    return {
+      width: withTiming(animatedWidth.value, {
+        duration: 500,
+        easing: Easing.bezier(0.5, 0.01, 0, 1),
+      }),
+      height: withTiming(animatedHeight.value, {
+        duration: 500,
+        easing: Easing.bezier(0.5, 0.01, 0, 1),
+      }),
+    };
+  });
 
   const boxContainerStyle = useAnimatedStyle(() => {
     return {
@@ -24,16 +42,28 @@ export default function ReMovement() {
   });
 
   return (
-    <React.Fragment>
+    <SafeAreaView style={{ flex: 1, alignItems: 'center' }}>
+      {/* <View style={[{ height: 100, width: 100, backgroundColor: 'red' }]} /> */}
+      <Animated.View style={[{ borderRadius: 12, height: 0, width: 0, backgroundColor: 'red' }, circleStyle]} />
+      <A.View style={[{ borderRadius: 12, height: animatedSize, width: animatedSize, backgroundColor: 'green' }]} />
       <Animated.View style={[{ height: 100, width: 100, backgroundColor: 'black', margin: 30 }, boxContainerStyle]}></Animated.View>
       <Button
         title='toogle'
         onPress={() => {
           randomWidth.value = Math.random() * 350;
           randomFontSize.value = 32;
+
+          animatedWidth.value = animatedWidth.value === 24 ? 0 : 24;
+          animatedHeight.value = animatedHeight.value === 24 ? 0 : 24;
+
+          A.timing(animatedSize, {
+            toValue: 24,
+            duration: 500,
+            useNativeDriver: false,
+          }).start();
         }}
       />
       <Animated.Text style={[textStyle]}>Hello</Animated.Text>
-    </React.Fragment>
+    </SafeAreaView>
   );
 }
